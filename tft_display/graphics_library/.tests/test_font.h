94606a38cd6bbbae6425c49a8018017870a3d5f8
patryk3211 <patrykmierzy@gmail.com>
2024-07-27 19:08:33 +0200

    Added copyright headers

--- tft_display/graphics_library/.tests/test_font.h
+++ tft_display/graphics_library/.tests/test_font.h
@@ -1,3 +1,22 @@
+/*
+	Name: test_font.h
+	Description: Graphics library for µCNC
+    Font used for unit testing
+
+	Copyright: Copyright (c) Patryk Mierzyński
+	Author: Patryk Mierzyński
+	Date: 27/07/2024
+
+	µCNC is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version. Please see <http://www.gnu.org/licenses/>
+
+	µCNC is distributed WITHOUT ANY WARRANTY;
+	Also without the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	See the	GNU General Public License for more details.
+*/
+
 #include "../fonts/font.h"
 
 const uint8_t TestFontBitmaps[] = {
--- tft_display/screens/startup.h
+++ tft_display/screens/startup.h
@@ -1,3 +1,21 @@
+/*
+	Name: startup.h
+	Description: Startup screen for TFT panels
+
+	Copyright: Copyright (c) Patryk Mierzyński
+	Author: Patryk Mierzyński
+	Date: 27/07/2024
+
+	µCNC is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version. Please see <http://www.gnu.org/licenses/>
+
+	µCNC is distributed WITHOUT ANY WARRANTY;
+	Also without the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+	See the GNU General Public License for more details.
+*/
+
 #ifndef TFT_DISPLAY_H
 #error "Please do not include this file manually"
 #endif
