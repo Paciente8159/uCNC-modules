6777f88e0bcd62d2369204e2392a668e7fb44cb7
Paciente8159 <Paciente8159@users.noreply.github.com>
2024-08-17 10:08:10 +0100

    fixed touch screen rotation
    - fixed touch screen rotation
    - simplified configuration on init function

--- touch_screen/touch_screen.h
+++ touch_screen/touch_screen.h
@@ -8,7 +8,8 @@ extern "C"
 
 #include <stdint.h>
 #include <stdbool.h>
-#include "../softspi.h"
+#include "src/cnc.h"
+#include "src/modules/softspi.h"
 
 /**
  * Parameters for calibration.
@@ -24,6 +25,7 @@ extern "C"
  */
 #define TOUCH_SCREEN_DFR_MODE 3
 #define TOUCH_SCREEN_SER_MODE 4
+
 #define TOUCH_SCREEN_READ_X 0xD0
 #define TOUCH_SCREEN_READ_Y 0x90
 
@@ -31,33 +33,9 @@ extern "C"
 #define TOUCH_ROTATION_90 1
 #define TOUCH_ROTATION_180 2
 #define TOUCH_ROTATION_270 3
-#define TOUCH_INVERT_Y 4
+#define TOUCH_INVERT_Y 0x80
 
-#ifndef TOUCH_ROTATION
-#define TOUCH_ROTATION (TOUCH_ROTATION_180 | TOUCH_INVERT_Y)
-#endif
-
-#if ((TOUCH_ROTATION == TOUCH_ROTATION_0) || (TOUCH_ROTATION == (TOUCH_ROTATION_270 | TOUCH_INVERT_Y)))
-#define CALC_X(width, x) (x)
-#define CALC_Y(height, y) (y)
-#elif ((TOUCH_ROTATION == TOUCH_ROTATION_90) || (TOUCH_ROTATION == (TOUCH_ROTATION_180 | TOUCH_INVERT_Y)))
-#define CALC_X(width, x) ((width) - (x))
-#define CALC_Y(height, y) (y)
-#elif ((TOUCH_ROTATION == TOUCH_ROTATION_180) || (TOUCH_ROTATION == (TOUCH_ROTATION_90 | TOUCH_INVERT_Y)))
-#define CALC_X(width, x) ((width) - (x))
-#define CALC_Y(height, y) ((height) - (y))
-#elif (TOUCH_ROTATION == TOUCH_ROTATION_270 || (TOUCH_ROTATION == (TOUCH_ROTATION_0 | TOUCH_INVERT_Y)))
-#define CALC_X(width, x) (x)
-#define CALC_Y(height, y) ((height) - (y))
-#else
-#error "Invalid touch screen rotation"
-#endif
-
-#define TOUCH_SCREEN_PARAMS_ROTATE 1
-#define TOUCH_SCREEN_PARAMS_INVERT_X 2
-#define TOUCH_SCREEN_PARAMS_INVERT_Y 4
-
-	void touch_screen_init(softspi_port_t* spiport, uint16_t width, uint16_t height, uint8_t cs_pin, uint8_t penirq_pin);
+	void touch_screen_init(softspi_port_t *spiport, uint16_t width, uint16_t height, uint8_t flags, uint8_t cs_pin, uint8_t penirq_pin);
 	void touch_screen_get_calibration(uint16_t *x1, uint16_t *y1, uint16_t *x2, uint16_t *y2);
 	void touch_screen_set_calibration(float adc_x1, float adc_y1, float adc_x2, float adc_y2);
 	void touch_screen_get_adc(uint16_t *adc_x, uint16_t *adc_y, uint8_t max_samples);
